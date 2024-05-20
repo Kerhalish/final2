8e42f33edfc644fde81468b7da094f85c4cc3a09
ubuntu <arkossy.mate1997@gmail.com>
2024-05-20 15:06:54 +0200

    Fix Docker build path in GitHub Actions

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# final2
+# Féléves Projekt Repository
+![Build Status](https://github.com/Kerhalish/Homework_CI-CD/actions/workflows/default.yml/badge.svg)
+
+
+## Projekt Leírás
+Ebben a repositoryban dolgozom a félév során elkészítendő feladatra. A projekt célja egy webes szolgáltatás létrehozása, amely képes képek és leírások feltöltésére és kezelésére, valamint autók automatikus detektálására a feltöltött képeken.
+
+### Feladat Leírása
+
+A félév során mindenkinek létre kell hoznia egy fejlesztői CI/CD környezetet a saját gépén és ennek használatával egy olyan web szolgáltatást kell létrehozni ami az alábbi funkciókat látja el:
+
+Kép és hozzá tartozó leírás feltöltése (kép és leírás páros tárolás)
+A feltöltött képen automatikus autó detektálás és a megtalált autók bekeretezésével a kép megjelenítése a weboldalon
+A weboldal “üzemeltetői” képesek legyenek feliratkozni az oldalra, azaz kapjanak értesítést az összes eddigi és az új feltöltött képekről úgy, hogy kiküldésre kerül számukra a képhez tartozó leírás és a rendszer által detektált autók száma a feltöltött képen
+
+
+### CI/CD Használata
+A projekt CI/CD részéhez GitHub Actions-t használok, amelyet a korábbi laborok során ismerkedtem meg.
+
+
+## Fontos Dátumok
+- `2024.04.14` - Fejlesztői dokumentáció feltöltése, mely tartalmazza a CI/CD környezetet.
+- `2024.04.28` - Fejlesztői dokumentáció feltöltése a képfeltöltés és autó detektálás funkcióval kapcsolatos leírásokkal.
+- `2024.05.12` - Végleges Fejlesztői dokumentáció feltöltése és jelentkezés a félév végi projekt bemutatásra.
+
+## Fejlesztői Dokumentáció
+A projekt minden lépéséről részletes fejlesztői dokumentációt készítek, amely tartalmazza a futási környezet, a CI/CD környezet, a fejlesztett kód, az adatszerkezetek, és a webszolgáltatás architektúrájának leírását.
---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from ultralytics import YOLO
+import os
+import json
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+app.secret_key = 'sdlkégmjsdéklgjsdlékjmlkdsgjnlknsld.m,fnáéaskldasd'
+UPLOAD_FOLDER = 'static/uploads'
+DETECTED_FOLDER = 'static/detected'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def save_descriptions(descriptions):
+    with open('descriptions.json', 'w') as f:
+        json.dump(descriptions, f)
+
+
+def load_descriptions():
+    try:
+        with open('descriptions.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+# Load descriptions when the app starts
+descriptions = load_descriptions()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    file = request.files['image']
+    description = request.form['description']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        descriptions[filename] = description  # Store the description
+        save_descriptions(descriptions)  # Save descriptions after updating
+        return redirect(url_for('detect_image', filename=filename))
+    else:
+        flash('Only JPG, JPEG, or PNG files can be uploaded.')
+        return redirect(url_for('index'))
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return render_template('display_image.html', filename=filename,
+                           image_url=url_for('static', filename=os.path.join('uploads', filename)))
+
+
+@app.route('/images')
+def list_images():
+    images = [f for f in os.listdir(UPLOAD_FOLDER) if os.path.isfile(os.path.join(UPLOAD_FOLDER, f))]
+    return render_template('images.html', images=images)
+
+
+@app.route('/detect/<filename>')
+def detect_image(filename):
+    source_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    model = YOLO('yolov8n.pt')  # Ensure the model file path is correct
+    results = model.predict(source_path, project=DETECTED_FOLDER, name='results', exist_ok=True, save=True)
+    names = model.names  # Get the model names dictionary
+
+    car_id = None
+    for key, value in names.items():
+        if value == 'car':
+            car_id = key
+            break
+
+    if car_id is not None:
+        car_count = sum(1 for box in results[0].boxes if box.cls == car_id)
+        if results:
+            descriptions[filename] += f" | Detected cars: {car_count}"  # Update the description
+            save_descriptions(descriptions)  # Save descriptions after updating
+            flash('Detection completed successfully.')
+        else:
+            flash('Detection failed.')
+    else:
+        flash('Car class not found in model.')
+
+    return redirect(url_for('display_image', filename=filename))
+
+
+@app.route('/detected/results')
+def detected_images():
+    image_files = os.listdir('static/detected/results')
+    return render_template('detected_images.html', images=image_files, descriptions=descriptions)
+
+
+@app.route('/admin')
+def admin():
+    images = [f for f in os.listdir(UPLOAD_FOLDER) if os.path.isfile(os.path.join(UPLOAD_FOLDER, f))]
+    return render_template('admin.html', images=images, descriptions=descriptions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
